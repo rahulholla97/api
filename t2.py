2da4c84d9c80446a5652e504213447419b9b6691
rahulholla97 <rahulholla1@gmail.com>
2023-01-31 11:34:56 +0530

    TTM Upload

--- t2.py
+++ t2.py
@@ -2,5 +2,6 @@ import requests
 import json
 input_from_api = requests.get('https://jsonplaceholder.typicode.com/todos/1')
 data = input_from_api.text
-print(json.loads(data))
-print('Hello TTM')
+aa=(json.loads(data))
+for k,v in aa.items():
+    print(f"{k} is assigned to {v}")
