1dbdec82f10fedbcc9db1064e8d8385181c6e198
rahulholla97 <rahulholla1@gmail.com>
2023-01-31 12:15:49 +0530

    deleted t2 in kumar

--- t2.py
+++
@@ -1,7 +0,0 @@
-import requests
-import json
-input_from_api = requests.get('https://jsonplaceholder.typicode.com/todos/1')
-data = input_from_api.text
-aa=(json.loads(data))
-for k,v in aa.items():
-    print(f"{k} is assigned to {v}")
