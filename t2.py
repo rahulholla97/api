7d925656ebfa8ea82d7ceb065f0aa500fc18f2f8
rahulholla97 <rahulholla1@gmail.com>
2023-01-31 09:20:55 +0530

    An api task

--- t2.py
+++ t2.py
@@ -1,3 +1,5 @@
-def start1():
-    print('Hello world!')
-start1()
+import requests
+import json
+input_from_api = requests.get('https://jsonplaceholder.typicode.com/todos/1')
+data = input_from_api.text
+print(json.loads(data))
