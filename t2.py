e4f9b32a4adf66b5451e439e62148c7e4e609315
rahulholla97 <rahulholla1@gmail.com>
2023-01-31 13:05:32 +0530

    Added some code

--- t2.py
+++ t2.py
@@ -3,5 +3,6 @@ import json
 input_from_api = requests.get('https://jsonplaceholder.typicode.com/todos/1')
 data = input_from_api.text
 aa=(json.loads(data))
+print('Welcome world')
 for k,v in aa.items():
     print(f"{k} is assigned to {v}")
